dc6d94656b6c87569d05d701913b7dad8cc87f0c
eskimo <1145400536@qq.com>
2024-03-17 22:45:46 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // build path
+  // base: '/mangosteen/', 
+  plugins: [vue()]
+})
