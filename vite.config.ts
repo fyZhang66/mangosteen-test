62a0e946404c10c2a311ea522b8b8da27becb04f
eskimo <1145400536@qq.com>
2024-03-18 15:40:25 +0800

    vite set github  base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   // build path
-  // base: '/mangosteen/', 
+  base: '/mangosteen-test/dist/', 
   plugins: [vue()]
 })
